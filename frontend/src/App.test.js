ee3b90a8f14f757aa1496f0f657015af7f7149e3
HonestJJ <nb5683@outlook.kr>
2022-01-21 00:18:11 +0900

    travis yml Added3

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,7 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
