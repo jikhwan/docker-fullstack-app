238fd43513e6048a59f816fa2cae67e7a25cff29
HonestJJ <nb5683@outlook.kr>
2022-01-21 00:27:46 +0900

    travis yml Added4

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,12 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
